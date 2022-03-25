68c7fb74724ba4040a7aa35716c684dc63d9de98
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 16:58:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -417,7 +417,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
         try:
             msg = await client.send_cached_media(
-                chat_id=REDIRECT_TO,
+                chat_id=AUTH_CHANNEL,
                 file_id=file_id,
                 caption=f_caption,
                 protect_content=True if ident == "filep" else False 
