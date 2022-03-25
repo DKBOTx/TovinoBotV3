8c29cc3769cc14596affe163a8f852a2dfa6777b
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 16:46:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/NEW-CHANNEL.git /NEW-CHANNEL
+  git clone https://github.com/Sreejithmadmax/deleterobot.git /NEW-CHANNEL
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /NEW-CHANNEL
+  git clone $UPSTREAM_REPO /deleterobot
 fi
-cd /NEW-CHANNEL
+cd /deleterobot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
