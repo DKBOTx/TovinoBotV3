af804abe1f3d6d6d7139edbbd00ad9093a3ebb67
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 16:57:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/deleterobot.git /NEW-CHANNEL
+  git clone https://github.com/Sreejithmadmax/deleterobot.git /deleterobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /deleterobot
