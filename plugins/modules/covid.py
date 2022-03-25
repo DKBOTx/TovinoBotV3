a1b48f2e3792016838aab9c6605c3f944de167f0
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 12:15:18 +0530

    Add files via upload

---
+++ plugins/modules/covid.py
@@ -0,0 +1,54 @@
+# @PaulWalker_TG
+
+import os
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+API = "https://api.sumanjay.cf/covid/?country="
+
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/telegram')]])
+
+
+@Client.on_message(filters.command("covid"))
+async def reply_info(bot, update):
+    query = update.text.split(None, 1)[1]
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=covid_info(query),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+
+
+def covid_info(country_name):
+    try:
+        r = requests.get(API + requote_uri(country_name.lower()))
+        info = r.json()
+        country = info['country'].capitalize()
+        active = info['active']
+        confirmed = info['confirmed']
+        deaths = info['deaths']
+        info_id = info['id']
+        last_update = info['last_update']
+        latitude = info['latitude']
+        longitude = info['longitude']
+        recovered = info['recovered']
+        covid_info = f"""--**Covid 19 Information**--
+
+Country : `{country}`
+Actived : `{active}`
+Confirmed : `{confirmed}`
+Deaths : `{deaths}`
+ID : `{info_id}`
+Last Update : `{last_update}`
+Latitude : `{latitude}`
+Longitude : `{longitude}`
+Recovered : `{recovered}`
+"""
+        return covid_info
+    except Exception as error:
+        return error
+
