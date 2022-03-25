335aaf339ffb1e1705bb23b3ffb284e818f2c165
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 15:05:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -149,7 +149,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase')
+            k = await query.message.edit('💔This Movie Not Found In DataBase Please Contact @PromotionMediator (admin) To add this File')
             await asyncio.sleep(10)
             await k.delete()
 
