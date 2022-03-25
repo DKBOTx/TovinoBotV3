56551e9d3d50660ab79406f0bd8659d6886cd353
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 21:04:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1132,65 +1132,57 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
 
 
 async def advantage_spell_chok(msg):
-    # query = re.sub(
-    #     r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
-    #     "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
-    # query = query.strip() + " movie"
-    # g_s = await search_gagala(query)
-    # g_s += await search_gagala(msg.text)
-    # gs_parsed = []
-    # if not g_s:
-    #     k = await msg.reply("I couldn't find any movie in that name.")
-    #     await asyncio.sleep(8)
-    #     await k.delete()
-    #     return
-    # regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
-    # gs = list(filter(regex.match, g_s))
-    # gs_parsed = [re.sub(
-    #     r'\b(\-([a-zA-Z-\s])\-\simdb|(\-\s)?imdb|(\-\s)?wikipedia|\(|\)|\-|reviews|full|all|episode(s)?|film|movie|series)',
-    #     '', i, flags=re.IGNORECASE) for i in gs]
-    # if not gs_parsed:
-    #     reg = re.compile(r"watch(\s[a-zA-Z0-9_\s\-\(\)]*)*\|.*",
-    #                      re.IGNORECASE)  # match something like Watch Niram | Amazon Prime
-    #     for mv in g_s:
-    #         match = reg.match(mv)
-    #         if match:
-    #             gs_parsed.append(match.group(1))
-    # user = msg.from_user.id if msg.from_user else 0
-    # movielist = []
-    # gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates https://stackoverflow.com/a/7961425
-    # if len(gs_parsed) > 3:
-    #     gs_parsed = gs_parsed[:3]
-    # if gs_parsed:
-    #     for mov in gs_parsed:
-    #         imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
-    #         if imdb_s:
-    #             movielist += [movie.get('title') for movie in imdb_s]
-    # movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
-    # movielist = list(dict.fromkeys(movielist))  # removing duplicates
-    # if not movielist:
-    #     k = await msg.reply("I couldn't find anything related to that. Check your spelling")
-    #     await asyncio.sleep(8)
-    #     await k.delete()
-    #     return
-    # SPELL_CHECK[msg.message_id] = movielist
-    # btn = [[
-    #     InlineKeyboardButton(
-    #         text=movie.strip(),
-    #         callback_data=f"spolling#{user}#{k}",
-    #     )
-    # ] for k, movie in enumerate(movielist)]
-    # btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    fmsg = await msg.reply("""😌 Hey, user!
-This Movie/Series Not Found In DataBase 😑
-
-Theater Print Not Available Here..! 😑
-Check if the DVD is out ... ??⁇😊""",
-        True,
-        # reply_markup=InlineKeyboardMarkup(btn)
-    )
-    await asyncio.sleep(DELETE_TIME)
-    await fmsg.delete()
+    query = re.sub(
+        r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
+        "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
+    query = query.strip() + " movie"
+    g_s = await search_gagala(query)
+    g_s += await search_gagala(msg.text)
+    gs_parsed = []
+    if not g_s:
+        k = await msg.reply("I couldn't find any movie in that name.")
+        await asyncio.sleep(8)
+        await k.delete()
+        return
+    regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
+    gs = list(filter(regex.match, g_s))
+    gs_parsed = [re.sub(
+        r'\b(\-([a-zA-Z-\s])\-\simdb|(\-\s)?imdb|(\-\s)?wikipedia|\(|\)|\-|reviews|full|all|episode(s)?|film|movie|series)',
+        '', i, flags=re.IGNORECASE) for i in gs]
+    if not gs_parsed:
+        reg = re.compile(r"watch(\s[a-zA-Z0-9_\s\-\(\)]*)*\|.*",
+                         re.IGNORECASE)  # match something like Watch Niram | Amazon Prime
+        for mv in g_s:
+            match = reg.match(mv)
+            if match:
+                gs_parsed.append(match.group(1))
+    user = msg.from_user.id if msg.from_user else 0
+    movielist = []
+    gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates https://stackoverflow.com/a/7961425
+    if len(gs_parsed) > 3:
+        gs_parsed = gs_parsed[:3]
+    if gs_parsed:
+        for mov in gs_parsed:
+            imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
+            if imdb_s:
+                movielist += [movie.get('title') for movie in imdb_s]
+    movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
+    movielist = list(dict.fromkeys(movielist))  # removing duplicates
+    if not movielist:
+        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        await asyncio.sleep(8)
+        await k.delete()
+        return
+    SPELL_CHECK[msg.message_id] = movielist
+    btn = [[
+        InlineKeyboardButton(
+            text=movie.strip(),
+            callback_data=f"spolling#{user}#{k}",
+        )
+    ] for k, movie in enumerate(movielist)]
+    btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
+    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+                    reply_markup=InlineKeyboardMarkup(btn))
 
 
 async def manual_filters(client, message, text=False):
