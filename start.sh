4876dcfe14c97db519e9c5f251ac06ca24d4fa3d
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-03-25 12:21:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://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:x-oauth-basic@github.com/THE-Emperor0/EvaMaria-Mod.git /EvaMaria
+  git clone https://github.com/Sreejithmadmax/NEW-CHANNEL.git /NEW-CHANNEL
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /NEW-CHANNEL
 fi
-cd /EvaMaria
+cd /NEW-CHANNEL
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
