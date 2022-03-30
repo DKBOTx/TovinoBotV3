f55379c0e4091038944988a07d9baa193b65c4b0
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-30 20:10:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /My-Dream
 fi
-cd /deleterobot
+cd /My-Dream
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
