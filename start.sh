09d06e611c01d162085ef814abac27c9e8fa6e08
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-30 20:03:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/My-Dream /My-Dream
+  git clone https://github.com/4kcinemas/My-Dream.git /My-Dream
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /My-Dream
