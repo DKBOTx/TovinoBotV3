8692b5e32bdd703efd6735e1df83a3dc3f2679f3
achukuttanTG <104365884+achukuttanTG@users.noreply.github.com>
2022-04-28 11:06:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -425,7 +425,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             msg = await client.send_cached_media(
                 chat_id=AUTH_CHANNEL,
                 file_id=file_id,
-                caption=f'<b>Hai 👋 {query.from_user.mention}</b> 😍\n\n<code>[DK_BOTx] {title}</code>\n\n⚠️ This file will be deleted in 5 minute as it has copyright ... !!!\n\nAfter moving from here to saved message or somewhere else, download ... !!!</b>',
+                caption=f'<b>Hai 👋 {query.from_user.mention}</b> 😍\n\n<code>[DK_BOTx] {title}</code>\n\n⚠️ This file will be deleted in 1 minute as it has copyright ... !!!\n\nAfter moving from here to saved message or somewhere else, download ... !!!</b>\n\n<b>@FILESEARCHxBOT</b>',
                 protect_content=True if ident == "filep" else False 
             )
             msg1 = await query.message.reply(
