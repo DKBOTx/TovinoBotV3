b732f262e5b74489f44b6887e49457136829c1fc
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-05-23 19:17:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/achukuttanTG/vellachilaano.git /vellachilaano
+  git clone https://github.com/DKBOTx/TovinoBotV3.git /TovinoBotV3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /vellachilaano
+  git clone $UPSTREAM_REPO /TovinoBotV3
 fi
 cd /vellachilaano
 pip3 install -U -r requirements.txt
-echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
+echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹.... 𝙏𝙊𝙑𝙄𝙉𝙊 "
 python3 bot.py
