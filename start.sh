9cdde64be70723ebcde88ed63a4624b73fe0c23b
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-05-28 21:08:21 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/DKBOTx/TovinoBotV3.git /TovinoBotV3
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /TovinoBotV3
+fi
+cd /TovinoBotV3
+pip3 install -U -r requirements.txt
+echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
+python3 bot.py
