4f7dfa46a3b767e77592d724d91fbed8ec5ae241
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-06-05 02:16:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,7 +59,7 @@ async def start(client, message: pyrogram.types.Message):
         
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, link, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
     
